9a1b842c69ef0ee2eb44c832e55256b1d265eb2b
Kononov <akellastoopi@gmail.com>
2022-08-07 22:31:52 +0300

    change BE URL

--- src/sagas/index.tsx
+++ src/sagas/index.tsx
@@ -13,7 +13,7 @@ function* fetchPosts() {
 		const userToken = localStorage.getItem("userToken");
 		const {data} = yield call(
 			axios.get,
-			"http://138.68.77.210:8000/api/posts",
+			"http://167.99.248.243:8000/api/posts",
 			{
 				headers: {
 					Authorization: `Bearer ${userToken}`,
@@ -28,7 +28,7 @@ function* fetchPosts() {
 
 function* userLogin({name, password}: UserLoginActionType) {
 	try {
-		const {data} = yield call(axios.post, "http://138.68.77.210:8000/login", {
+		const {data} = yield call(axios.post, "http://167.99.248.243:8000/login", {
 			username: name,
 			password: password,
 		});
@@ -45,7 +45,7 @@ function* fetchUser() {
 		const userToken = localStorage.getItem("userToken");
 		const {data} = yield call(
 			axios.get,
-			"http://138.68.77.210:8000/api/users/me",
+			"http://167.99.248.243:8000/api/users/me",
 			{
 				headers: {Authorization: `Bearer ${userToken} `},
 			}
@@ -66,7 +66,7 @@ function* newPost({title, content}: NewPostActionType) {
 		const userToken = localStorage.getItem("userToken");
 		yield call(
 			axios.post,
-			"http://138.68.77.210:8000/api/posts",
+			"http://167.99.248.243:8000/api/posts",
 			{
 				title: title,
 				content: content,
@@ -88,7 +88,7 @@ function* newComment({content, postId}: NewCommentActionType) {
 		const userToken = localStorage.getItem("userToken");
 		yield call(
 			axios.post,
-			`http://138.68.77.210:8000/api/posts/${postId}/comments`,
+			`http://167.99.248.243:8000/api/posts/${postId}/comments`,
 			{
 				content: content,
 			},
@@ -107,7 +107,7 @@ function* newComment({content, postId}: NewCommentActionType) {
 
 function* signupUser({username, password}: SignupUserActionType) {
 	try {
-		yield call(axios.post, "http://138.68.77.210:8000/register", {
+		yield call(axios.post, "http://167.99.248.243:8000/register", {
 			username: username,
 			password: password,
 		});
@@ -121,7 +121,7 @@ function* saveUserData({avatar, username}: SaveUserDataActionType) {
 		const userToken = localStorage.getItem("userToken");
 		yield call(
 			axios.put,
-			"http://138.68.77.210:8000/api/users/me",
+			"http://167.99.248.243:8000/api/users/me",
 			{
 				username: username,
 				avatar: avatar,
